80ef35a5dd391ca742867dfe5901f5eb81d47b77
Steven <stevendawn98@gmail.com>
2022-04-12 13:00:25 -0400

    commit tree 0d164f08919e6bb8fddab76176ce7d5cde2b4127

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    console.log(string);
+    console.log(string.toLowerCase());
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    }
+    // console.log(string.toUpperCase)
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+    if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+}
+
